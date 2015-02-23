5052cfa765098b55cc44bf84a248c416a2d74c5f
Pizzalol <siprak214@hotmail.com>
2015-02-23 17:17:06 +0100

    Finished fire elemental sounds and particles, finished water elemental multishot

---
+++ game/dota_addons/twrpg/scripts/vscripts/heroes/hero_elementalist/units/water_elemental/multishot.lua
@@ -0,0 +1,58 @@
+--[[Author: Pizzalol
+	Creates additional attack projectiles for units within the targets attack range]]
+function multishot_start( keys )
+	local caster = keys.caster
+	local caster_location = caster:GetAbsOrigin()
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+
+	local radius = caster:GetAttackRange()
+	local target_type = ability:GetAbilityTargetType()
+	local target_team = ability:GetAbilityTargetTeam()
+	local target_flags = ability:GetAbilityTargetFlags()
+	local attack_target = caster:GetAttackTarget()
+
+	local max_targets = ability:GetLevelSpecialValueFor("max_targets", ability_level)
+	local multishot_projectile = keys.multishot_projectile
+
+	local multishot_targets = FindUnitsInRadius(caster:GetTeam(), caster_location, nil, radius, target_team, target_type, target_flags, FIND_CLOSEST, false)
+
+	-- Create projectiles for units that are not the targets current attack target
+	for _,v in pairs(multishot_targets) do
+		if v ~= attack_target then
+			local projectile_info = 
+			{
+				EffectName = multishot_projectile,
+				Ability = ability,
+				vSpawnOrigin = caster_location,
+				Target = v,
+				Source = caster,
+				bHasFrontalCone = false,
+				iMoveSpeed = caster:GetProjectileSpeed(),
+				bReplaceExisting = false,
+				bProvidesVision = false
+			}
+			ProjectileManager:CreateTrackingProjectile(projectile_info)
+			max_targets = max_targets - 1
+		end
+		-- If we reached the maximum amount of targets then break the loop
+		if max_targets == 0 then break end
+	end
+end
+
+-- Apply the auto attack damage to the hit unit
+function multishot_damage( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	local damage_table = {}
+
+	damage_table.attacker = caster
+	damage_table.victim = target
+	damage_table.damage_type = ability:GetAbilityDamageType()
+	damage_table.ability = ability
+	damage_table.damage = caster:GetAttackDamage()
+
+	ApplyDamage(damage_table)
+end
