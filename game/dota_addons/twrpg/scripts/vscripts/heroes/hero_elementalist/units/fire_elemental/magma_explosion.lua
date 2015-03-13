44430492c91bb4b2af1f5b0e044817d1159e5dbc
Pizzalol <siprak214@hotmail.com>
2015-03-13 22:57:56 +0100

    Finished all 4 basic elementals

--- game/dota_addons/twrpg/scripts/vscripts/heroes/hero_elementalist/units/fire_elemental/magma_explosion.lua
+++ game/dota_addons/twrpg/scripts/vscripts/heroes/hero_elementalist/units/fire_elemental/magma_explosion.lua
@@ -1,55 +1,10 @@
 --[[Author: Pizzalol
-	Increases attack speed if attacking the same target over and over]]
-function blazing_haste( keys )
-	local caster = keys.caster
-	local target = keys.target
-	local ability = keys.ability
-	local ability_level = ability:GetLevel() - 1
-	local modifier = keys.modifier
-	local max_stacks = ability:GetLevelSpecialValueFor("max_stacks", ability_level)
-
-	-- Check if we have an old target
-	if caster.blazing_haste_target then
-		-- Check if that old target is the same as the attacked target
-		if caster.blazing_haste_target == target then
-			-- Check if the caster has the attack speed modifier
-			if caster:HasModifier(modifier) then
-				-- Get the current stacks
-				local stack_count = caster:GetModifierStackCount(modifier, ability)
-
-				-- Check if the current stacks are lower than the maximum allowed
-				if stack_count < max_stacks then
-					-- Increase the count if they are
-					caster:SetModifierStackCount(modifier, ability, stack_count + 1)
-				end
-			else
-				-- Apply the attack speed modifier and set the starting stack number
-				ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
-				caster:SetModifierStackCount(modifier, ability, 1)
-			end
-		else
-			-- If its not the same target then set it as the new target and remove the modifier
-			caster:RemoveModifierByName(modifier)
-			caster.blazing_haste_target = target
-		end
-	else
-		caster.blazing_haste_target = target
-	end
-end
-
---[[Author: Pizzalol
-	Triggers upon the death of the caster
-	Deals damage depending on distance]]
-function blazing_haste_explosion( keys )
+	Deals damage depending on distance and kills the caster]]
+function magma_explosion( keys )
 	local caster = keys.caster
 	local caster_location = caster:GetAbsOrigin()
-	local attacker = keys.attacker
 	local ability = keys.ability
 	local ability_level = ability:GetLevel() - 1
-
-	-- Sound and visual
-	local sound = keys.sound
-	local explosion_particle = keys.explosion_particle
 
 	local radius_min = ability:GetLevelSpecialValueFor("radius_min", ability_level)
 	local radius_max = ability:GetLevelSpecialValueFor("radius_max", ability_level) 
@@ -73,14 +28,6 @@ function blazing_haste_explosion( keys )
 	damage_table.ability = ability
 	damage_table.damage_type = ability:GetAbilityDamageType()
 
-	-- Play the visual and sound if the caster wasnt the killer
-	if attacker ~= caster then
-		EmitSoundOn(sound, caster)
-
-		local particle = ParticleManager:CreateParticle(explosion_particle, PATTACH_ABSORIGIN_FOLLOW, caster) 
-		ParticleManager:ReleaseParticleIndex(particle)
-	end
-
 	-- Find the units inside the smaller radius
 	local radius_min_targets = FindUnitsInRadius(caster:GetTeam(), caster_location, nil, radius_min, target_team, target_type, target_flag, FIND_CLOSEST, false)
 	-- Find the units inside the bigger radius
@@ -99,4 +46,7 @@ function blazing_haste_explosion( keys )
 		damage_table.victim = unit
 		ApplyDamage(damage_table)
 	end
+
+	-- Kill the caster
+	caster:ForceKill(true)
 end
